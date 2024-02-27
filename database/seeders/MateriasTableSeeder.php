edb211849386b5e36f22f0474b57e43978823a89
joseg-alvesg <cinhoalves85@gmail.com>
2024-02-27 12:29:05 -0300

    improve: seeder with images to example

--- database/seeders/MateriasTableSeeder.php
+++ database/seeders/MateriasTableSeeder.php
@@ -7,22 +7,66 @@ use Illuminate\Support\Facades\DB;
 
 class MateriasTableSeeder extends Seeder
 {
-    private $_materias = [
-        [
-            'titulo' => 'Título da Matéria 1',
-            'descricao' => 'Descrição da Matéria 1',
-            'texto_completo' => 'Texto completo da Matéria 1',
-            'imagem' => 'url.da.imagem.com',
-        ],
-        [
-            'titulo' => 'Título da Matéria 2',
-            'descricao' => 'Descrição da Matéria 2',
-            'texto_completo' => 'Texto completo da Matéria 2',
-            'imagem' => 'url.da.imagem.com',
-        ],
-    ];
-    public function run(): void
+
+    public function __construct()
     {
-        DB::table('materias')->insert($this->_materias);
+        $this->deleteImage('images');
+    }
+
+    public function run(): void
+    { $materias = [
+            [
+                'titulo' => 'Título da Matéria 1',
+                'descricao' => 'Descrição da Matéria 1',
+                'texto_completo' => 'Texto completo da Matéria 1',
+                'imagem' => $this->saveImage('example_images/sun.jpg'),
+            ],
+            [
+                'titulo' => 'Título da Matéria 2',
+                'descricao' => 'Descrição da Matéria 2',
+                'texto_completo' => 'Texto completo da Matéria 2',
+                'imagem' => $this->saveImage('example_images/blue_bird.jpg'),
+            ],
+            [
+                'titulo' => 'Título da Matéria 3',
+                'descricao' => 'Descrição da Matéria 3',
+                'texto_completo' => 'Texto completo da Matéria 3',
+                'imagem' => $this->saveImage('example_images/green_road.jpg'),
+            ]
+        ];
+
+        DB::table('materias')->insert($materias);
+    }
+
+    private function deleteImage()
+    {
+        $destinationPath = public_path('images');
+        if(file_exists($destinationPath)) {
+            $files = glob($destinationPath . '/*');
+            foreach($files as $file) {
+                if(is_file($file)) {
+                    unlink($file);
+                } else {
+                    rmdir($file);
+                }
+            }
+            rmdir($destinationPath);
+        }
+
+    }
+
+    private function saveImage($imagePath)
+    {
+        $imageContent = file_get_contents(public_path($imagePath));
+        $extension = pathinfo($imagePath, PATHINFO_EXTENSION);
+        $hash = md5($imageContent . strtotime('now')) . '.' . $extension;
+        $destinationPath = public_path('images');
+
+        if (!file_exists($destinationPath)) {
+            mkdir($destinationPath, 0777, true);
+        }
+
+        file_put_contents($destinationPath . '/' . $hash, $imageContent);
+        return $hash;
     }
 }
