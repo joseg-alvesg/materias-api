98d743aa69aab89161fe807bbc98aa84a397127e
joseg-alvesg <cinhoalves85@gmail.com>
2024-02-27 16:55:52 -0300

    commands to migrate and seed in container

---
+++ app/Console/Commands/Seeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Seeder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:seeder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'populate the database with examples';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $dockerCommand = 'docker exec -it laravel php artisan db:seed --class=MateriasTableSeeder --force';
+        $this->info('Running seeder...');
+        exec($dockerCommand, $output);
+    }
+}
